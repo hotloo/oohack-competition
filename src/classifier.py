3680afda1dc41abfa2bf51792f1809ba51a86c0e
Antonio Macías Ojeda <antonio.macias.ojeda (at) gmail (dot) com>
2012-10-16 18:30:45 +0300

    All working

--- src/classifier.py
+++ src/classifier.py
@@ -34,30 +34,37 @@ def normalize_hashes(X, columns):
 def evaluate(target, predicted):
     total = len(target)
     correct = sum(target == predicted)
-    # tp = sum(np.logical_and(target, predicted))
-    # fp = sum(np.logical_and(target == 0, predicted == 1))
-    # fn = sum(np.logical_and(target == 1, predicted == 0))
-    # print total, correct, tp, fp, fn
     accuracy = correct / total
-    # precision = tp / (tp + fp)
-    # recall = tp / (tp + fn)
-    # f1 = 2 * precision * recall / (precision + recall)
-    # return (accuracy, recall, precision, f1)
     return accuracy
 
 
 def main():
     print 'Loading train data'
     dataset = np.load('../data/oohack_train.npy')
-    print 'Loading test data'
+    # print 'Loading test data'
     # testset = np.load('../data/oohack_test.npy')
 
-    target = np.array([x[0] for x in dataset])
-    train = np.array([x[1:] for x in dataset])
+    data_target = np.array([x[0] for x in dataset])
+    data = np.array([x[1:] for x in dataset])
     # test_target = np.array([x[0] for x in testset])
     # test = np.array([x[1:] for x in testset])
 
     print 'Data loaded, starting pre-processing'
+    #Dirty, no time
+    indexes = []
+    others = []
+    i = 0
+    while i < len(data_target):
+        if data_target[i] == 1:
+            indexes.append(i)
+        else:
+            others.append(i)
+        i += 1
+    indexes = np.random.permutation(indexes)
+    others = np.array(others)
+    train = np.concatenate((data[indexes[0:7000]], data[others]))
+    target = np.concatenate((data_target[indexes[0:7000]], data_target[others]))
+
     nan_cols = [2]
     hashed_cols = []
     normalize_nan(train, nan_cols)
@@ -65,9 +72,11 @@ def main():
     scaler = preprocessing.Scaler()
     train = scaler.fit_transform(train)
 
-    # normalize_nan(test, nan_cols)
-    # normalize_hashes(test, hashed_cols)
-    # test = scaler.transform(test)
+    test = data[indexes[7000:]]
+    test_target = data_target[indexes[7000:]]
+    normalize_nan(test, nan_cols)
+    normalize_hashes(test, hashed_cols)
+    test = scaler.transform(test)
 
     n_features = train.shape[1]
     print 'Post pre-processing features:', n_features
@@ -76,21 +85,6 @@ def main():
     clf = RandomForestClassifier(n_estimators=100,
                                  max_features='sqrt',
                                  max_depth=None, n_jobs=-1)
-
-    # Some good default values for classification
-    #clf = svm.SVC(cache_size=512, gamma=0.001, kernel='poly', degree=3,
-                   #class_weight='auto')
-    #clf = svm.SVC(cache_size=512, gamma=0.001, kernel='rbf', degree=3,
-                   #class_weight='auto')
-    #clf = svm.SVC(cache_size=512, gamma=0.001, kernel='linear',
-                   #class_weight='auto')
-    #clf = LinearSVC(penalty='l1', dual=False, tol=1e-3)
-    #clf = LinearSVC(loss='l2', penalty='l1', dual=False, tol=1e-3)
-    #clf = LinearSVC(loss='l2', penalty='l2', dual=False, tol=1e-3)
-
-    #clf = KNeighborsClassifier(n_neighbors=10, warn_on_equidistant=False)
-    #clf = linear_model.LogisticRegression()
-    #clf = MultinomialNB(alpha=.01)
 
     print 'Starting Cross-Validation training and evaluation'
     cvround = 1
@@ -103,21 +97,18 @@ def main():
         results = np.concatenate((results, pclass))
         cvround += 1
     evaluation = evaluate(target, results)
-    print 'Estimated CV accuracy: ', evaluation
-    # print 'Estimated CV accuracy: ', evaluation[0]
-    # print 'Estimated CV precision: ', evaluation[1]
-    # print 'Estimated CV recall: ', evaluation[2]
-    # print 'Estimated CV F1 score: ', evaluation[3]
+    print 'Estimated CV accuracy:', evaluation
+    print 'Total dead:', sum(target == 0)
+    print 'Total just alive:', sum(target == 1)
+    print 'Total acquired:', sum(target == 2)
+    print 'Total in IPO:', sum(target == 3)
 
-
-    # print 'Retraining with everything and saving results'
-    # pclass = clf.fit(train, target).predict(test)
-    # evaluation = evaluate(test_target, pclass)
-    # print 'Generalization accuracy: ', evaluation[0]
-    # print 'Generalization precision: ', evaluation[1]
-    # print 'Generalization recall: ', evaluation[2]
-    # print 'Generalization F1 score: ', evaluation[3]
-    # np.savetxt('../data/result.csv', pclass, delimiter=',', fmt='%d')
+    print 'Predictions on the alive:'
+    clf.fit(train, target)
+    results = clf.predict(test)
+    print 'Predicted dying:', sum(np.logical_and(test_target == 1, results == 0))
+    print 'Predicted being acquired:', sum(np.logical_and(test_target == 1, results == 2))
+    print 'Predicted entering IPO:', sum(np.logical_and(test_target == 1, results == 3))
 
 if __name__ == "__main__":
     main()
